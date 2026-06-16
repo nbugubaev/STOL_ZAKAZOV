1706f3e1be9ad274a51f50bfa2492a948c2f1859
nbugubaev <nbugubaev@gmail.com>
2026-06-16 21:57:54 +0500

    Delete src/form.jsx

--- src/form.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import TicketForm from './TicketForm.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <TicketForm />
-  </React.StrictMode>,
-)
